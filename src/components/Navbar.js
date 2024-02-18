a54f30090e651839ca799e09f2c8d9596de06413
Michael Mwai <mwai.michael@strathmore.edu>
2024-02-18 13:11:33 +0300

    navbar complete

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,12 +3,29 @@
 /* eslint-disable no-unused-vars */
 import React, {useState} from 'react';
 import { Link } from 'react-router-dom';
+import { Button } from './Button';
+import './Navbar.css';
 
 function Navbar() {
   const [click, setClick] = useState(false);
+  const [button, setButton] = useState(true);
 
   const handleClick = () => setClick(!click);
   const closeMobileMenu = () => setClick(!false);
+
+  const showButton = () =>
+  {
+    if(window.innerWidth <= 960)
+    {
+      setButton(false)
+    }
+    else
+    {
+      setButton(true)
+    }
+  };
+  
+  window.addEventListener('resize', showButton);
 
   return (
     <>
@@ -44,6 +61,7 @@ function Navbar() {
             </li>
 
             </ul>
+            {button && <Button buttonStyle='btn--outline'>SIGN UP</Button>}
         </div>
     </nav>
     </>
