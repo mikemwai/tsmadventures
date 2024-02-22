451b87fe18f633a96f316eeb96a5230bb5658a2f
mikemwai <mwai.michael@strathmore.edu>
2024-02-22 17:31:05 +0300

    bug fixed:background image

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -59,11 +59,12 @@ function Footer
             </div>
             <section className="social-media">
                 <div className="social-media-wrap">
-                    <div className="footer-logo">
+                    {/* <div className="footer-logo">
                         <Link to='/' className="social-logo">
                             tsmadventures <i className="fab fa-typo3"></i>
                         </Link>
-                    </div>
+                    </div> */}
+                    <div className="footer-logo"></div>
                     <small className="website-rights">tsmaventures © {new Date().getFullYear()}</small>
                     <div className="social-icons">
                         <Link className="social-icon-link facebook" to="/" target="_blank" aria-label="Facebook">
