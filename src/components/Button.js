02b56113c0dc2f2a4f8f22623e63b4ba2aa2391b
mikemwai <mwai.michael@strathmore.edu>
2024-03-11 13:27:15 +0300

    minor updates

--- src/components/Button.js
+++ src/components/Button.js
@@ -2,10 +2,10 @@ import React from 'react'
 import './Button.css';  
 import {Link} from 'react-router-dom';
 
-const STYLES = ['btn--primary', 'btn--outline', 'btn--secondary'];
+const STYLES = ['btn--primary', 'btn--outline', 'btn--secondary', 'btn--icon'];
 const SIZES = ['btn--medium', 'btn--large'];
 
-export const Button = ({children, type, onClick, buttonStyle, buttonSize, buttonLink = '/sign-up'}) => {
+export const Button = ({children, type, onClick, buttonStyle, buttonSize, buttonLink = '/sign-in'}) => {
     const checkButtonStyle = STYLES.includes(buttonStyle) ? buttonStyle : STYLES[0];
     const checkButtonSize = SIZES.includes(buttonSize) ? buttonSize : SIZES[0];
 
